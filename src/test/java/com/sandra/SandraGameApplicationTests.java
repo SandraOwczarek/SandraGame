f9e53bb05c3df4ff352c3209c1aa36dcdddc5b55
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-01-18 15:19:28 +0100

    gra Sandra

---
+++ src/test/java/com/sandra/SandraGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandraGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
