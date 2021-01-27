ee2dc657fea76c667be94476bb286b132f121a4a
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-01-27 19:55:07 +0100

    added options to menu

--- src/main/java/com/sandra/GameFrame.java
+++ src/main/java/com/sandra/GameFrame.java
@@ -5,12 +5,22 @@ import java.awt.*;
 
 
 class MenuGame extends JFrame  {
-    JMenu menu ;
-
+    JMenu menu, submenu;
+    JMenuItem i1, i2, i3, i4, i5;
     GamePanel panel; // canvas gdzie bedziemy malowac gre
     MenuGame() { // konstruktor
+
         JMenuBar mb=new JMenuBar();
         menu = new JMenu ("Menu");
+        submenu = new JMenu("Level ");
+        i1= new JMenuItem ("Open New Game");
+        i2= new JMenuItem ("Save game");
+        i3= new JMenuItem ("Easy");
+        i4= new JMenuItem ("Medium");
+        i5= new JMenuItem ("Hard");
+        menu.add(i1); menu.add(i2);
+        submenu.add(i3);submenu.add(i4); submenu.add(i5);
+        menu.add(submenu);
         panel = new GamePanel(); // dodajemy rzeczy do naszej planszy
         this.add(panel);
         this.setTitle("Sandra Game");
